ccee110ce9ca9c0840360a125cb56d7cc4bae7d5
Joice <c14230225@john.petra.ac.id>
2025-09-30 20:14:02 +0700

    Design UI

--- app/src/main/java/latihan/c14230225/latihanuts/MainActivity.kt
+++ app/src/main/java/latihan/c14230225/latihanuts/MainActivity.kt
@@ -1,10 +1,13 @@
 package latihan.c14230225.latihanuts
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val _txtSatu = findViewById<TextView>(R.id.txtInSatu)
+        val _txtDua = findViewById<TextView>(R.id.txtInDua)
+        val _btnAdd = findViewById<Button>(R.id.btnTambah)
+
+
+    }
+
+    private fun replaceFragmentOperasi(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frag_operasi, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    private fun replaceFragmentHasil(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frag_hasil, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
